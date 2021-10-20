cc518dda89880217bf1210eb4404a6950054d5da
Automator <79113112+Khormologia@users.noreply.github.com>
2021-10-21 00:04:31 +0200

    loging

--- main.py
+++ main.py
@@ -2,14 +2,20 @@
 from modules.spotify_db import *
 from modules.youtube_to_mp3 import *
 from modules.plex import *
+from modules.logger import *
+
+log("Process 1")
 print('\nUpdating the playlists')
 spotify_c().update_playlists()
 
+log("Process 2")
 print('\nUpdating the locations')
 build_track_location()
 
+log("Process 3")
 print('\nDownload missing')
 download_missing()
 
+log("Process 4")
 print('\nConnecting to plex & updating the playlists')
 plex_c()
